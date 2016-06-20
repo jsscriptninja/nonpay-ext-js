201a4e9b991e15902fde210437f3c012b352b7b1
jsscriptninja <sellmystuff2012@gmail.com>
2016-06-20 14:23:11 +0530

    Update script.js

--- fliteairtaxi.com/script.js
+++ fliteairtaxi.com/script.js
@@ -1,4 +1,4 @@
 /* fliteairtaxi.com script */
 (function($){
-  $('#menu-main-menu').append('<li id="" class="mega-menu-item nav-item menu-item-depth-0"><a href="#" class="menu-link main-menu-link item-title" id="">Taxi</a></li>');
+  //$('#menu-main-menu').append('<li id="" class="mega-menu-item nav-item menu-item-depth-0"><a href="#" class="menu-link main-menu-link item-title" id="">Taxi</a></li>');
 })(jQuery);
