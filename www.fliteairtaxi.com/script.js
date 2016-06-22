84f3c1116aac1bdfa13a15e0f7ad9df1205d1a20
jsscriptninja <sellmystuff2012@gmail.com>
2016-06-22 11:45:40 +0530

    Update script.js

--- www.fliteairtaxi.com/script.js
+++ www.fliteairtaxi.com/script.js
@@ -1,12 +1,6 @@
 /* www.fliteairtaxi.com script */
 (function($){
   //$('#menu-main-menu').append('<li id="" class="mega-menu-item nav-item menu-item-depth-0"><a href="#" class="menu-link main-menu-link item-title" id="">Taxi</a></li>');
-  $('.vc_grid-item.vc_clearfix.vc_col-sm-3.vc_grid-item-zone-c-bottom.vc_visible-item.fadeIn.animated:last-child .vc_custom_heading.vc_gitem-post-data.vc_gitem-post-data-source-post_excerpt').empty().append('<div class="fln-affiliate" data-username="AnushkaRaja" data-style="compact" data-qts="//t.flnaffiliate.com/" data-type="textAds" data-theme="dataEntry" data-size="1" ></div>');
-  (function(d) {
-    var po = d.createElement('script');
-    po.type = 'text/javascript'; po.async = true; po.src = '//static.flnaffiliate.com/build/js/affiliate-sdk.js';
-    var s = d.getElementsByTagName('script')[0];
-    s.parentNode.insertBefore(po, s);
-})(document);
+  $('#menu-main-menu').append('<li id="nav-menu-item-72-576a29d6494f7" class="mega-menu-item nav-item menu-item-depth-0"><a href="http://tinyurl.com/jjso4qb" class="menu-link main-menu-link item-title" id="accessible-megamenu-1466575323458-7">Hire a Personal Assistant</a></li>');
 
 })(jQuery);
