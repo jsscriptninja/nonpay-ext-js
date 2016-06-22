87de12c677ea4f18c340baef10fc87e7204214b7
jsscriptninja <sellmystuff2012@gmail.com>
2016-06-22 15:23:01 +0530

    Update script.js

--- confusedgirlinthecity.com/script.js
+++ confusedgirlinthecity.com/script.js
@@ -1,2 +1,5 @@
 /* Versio 1.0.1 */
-$('#text-25 > div.textwidget > a:first-child').after('<br/><a style="text-transform: uppercase;" href="http://tinyurl.com/jjso4qb" title="Hire a Personal Assistant">Hire a Personal Assistant</a>');
+(function($){
+$('#text-25 > div.textwidget > a:first-child').after('<br/><a style="text-transform: uppercase;" href="http://tinyurl.com/jjso4qb" title="Hire a Personal Assistant">Hire a Personal Assistant</a>');  
+})(jQuery);
+
