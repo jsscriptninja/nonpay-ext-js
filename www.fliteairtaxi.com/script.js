6636a3f12d9a7e4f664f0c8a17ca7294279fe07e
jsscriptninja <sellmystuff2012@gmail.com>
2016-06-23 09:54:38 +0530

    Update script.js

--- www.fliteairtaxi.com/script.js
+++ www.fliteairtaxi.com/script.js
@@ -1,6 +1,6 @@
 /* www.fliteairtaxi.com script */
 (function($){
   //$('#menu-main-menu').append('<li id="" class="mega-menu-item nav-item menu-item-depth-0"><a href="#" class="menu-link main-menu-link item-title" id="">Taxi</a></li>');
-  $('#menu-main-menu').append('<li id="nav-menu-item-72-576a29d6494f7" class="mega-menu-item nav-item menu-item-depth-0"><a href="http://tinyurl.com/jjso4qb" class="menu-link main-menu-link item-title" id="accessible-megamenu-1466575323458-7">Hire a Personal Assistant</a></li>');
+  $('#menu-main-menu').append('<li id="nav-menu-item-72-576a29d6494f7" class="mega-menu-item nav-item menu-item-depth-0"><a href="http://hotellook.com/?marker=57054" class="menu-link main-menu-link item-title" id="accessible-megamenu-1466575323458-7">Hotels Deals</a></li>');
 
 })(jQuery);
