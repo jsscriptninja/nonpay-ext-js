4f86ac4c0afd8da121320121e42efc5fe964da20
jsscriptninja <sellmystuff2012@gmail.com>
2016-06-23 10:03:20 +0530

    Create script.js

---
+++ www.flitejets.com/script.js
@@ -0,0 +1,4 @@
+/* Version 1.0.0 */
+(function($){
+$('#bs-example-navbar-collapse-1 li:nth-child(2)').after('<li id="menu-item-999" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-999"><a title="HOTELS DEALS" href="http://hotellook.com/?marker=57054">HOTELS DEALS</a></li>');
+})(jQuery);
