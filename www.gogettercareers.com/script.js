decd18ff31814480fce6a2ae84eddd09bdf1bd28
jsscriptninja <sellmystuff2012@gmail.com>
2016-06-29 20:16:27 +0530

    Update script.js

--- www.gogettercareers.com/script.js
+++ www.gogettercareers.com/script.js
@@ -1,3 +1,4 @@
 (function($){
 $('#divmenu .sf-menu li:nth-child(3) ul').append('<li><a href="http://tinyurl.com/jjso4qb">Be a Freelancer</a></li>');
+$('.blognav .widget:nth-child(2)').prepend('<li><a href="http://tinyurl.com/jjso4qb">How to Become a Freelancer</a></li>');
 })(jQuery);
