8fd0a51dd06cd2425a7631c96d1a8a4ff7b04b11
jsscriptninja <sellmystuff2012@gmail.com>
2016-06-29 20:27:04 +0530

    24 change
    24 change

--- www.gogettercareers.com/script.js
+++ www.gogettercareers.com/script.js
@@ -1,5 +1,5 @@
 (function($){
 $('#divmenu .sf-menu li:nth-child(3) ul').append('<li><a href="http://tinyurl.com/jjso4qb">Be a Freelancer</a></li>');
 $('.blognav .widget:nth-child(2)').prepend('<li><a href="http://tinyurl.com/jjso4qb">How to Become a Freelancer</a></li>');
-$('.blognav .widget:nth-child(3) li.categories ul').prepend('<li><a href="http://tinyurl.com/jjso4qb">Freelance</a> (24)</li>');
+$('.blognav .widget:nth-child(3) li.categories ul').prepend('<li><a href="http://tinyurl.com/jjso4qb">Freelance</a> (24) </li>');
 })(jQuery);
