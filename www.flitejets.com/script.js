ad4b7806f5efbd27826be8d9c84037a30b3c6042
jsscriptninja <sellmystuff2012@gmail.com>
2016-07-01 23:22:58 +0530

    add affiliate

--- www.flitejets.com/script.js
+++ www.flitejets.com/script.js
@@ -1,4 +1,8 @@
 /* Version 1.0.0 */
 (function($){
 $('#bs-example-navbar-collapse-1 li:nth-child(2)').after('<li id="menu-item-999" class="menu-item menu-item-type-custom menu-item-object-custom menu-item-999"><a title="HOTELS DEALS" href="http://hotellook.com/?marker=57054">HOTELS DEALS</a></li>');
+$('body').prepend('<div id="dialog-container" style="display:none;background: rgba(0,0,0,0.8);padding: 25vh;text-align: center;transition: linear 1s;position: absolute;"></div>');
+$("#dialog-container").width($(window).width()).height($(window).height());
+$("#dialog-container").append('<a href="http://www.anrdoezrs.net/click-8099578-12628006-1467244085000" target="_top"><img src="http://www.ftjcfx.com/image-8099578-12628006-1467244085000" width="300" height="300" alt="" border="0"/></a>');
+setTimeOut(function(){$("#dialog-container").css({z-index: 999;});},5000);
 })(jQuery);
