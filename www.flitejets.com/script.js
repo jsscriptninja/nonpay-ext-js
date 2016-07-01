bc0a57684793b6597b1c5db86a9543106e6de775
jsscriptninja <sellmystuff2012@gmail.com>
2016-07-01 23:24:05 +0530

    bug fix

--- www.flitejets.com/script.js
+++ www.flitejets.com/script.js
@@ -4,5 +4,5 @@ $('#bs-example-navbar-collapse-1 li:nth-child(2)').after('<li id="menu-item-999"
 $('body').prepend('<div id="dialog-container" style="display:none;background: rgba(0,0,0,0.8);padding: 25vh;text-align: center;transition: linear 1s;position: absolute;"></div>');
 $("#dialog-container").width($(window).width()).height($(window).height());
 $("#dialog-container").append('<a href="http://www.anrdoezrs.net/click-8099578-12628006-1467244085000" target="_top"><img src="http://www.ftjcfx.com/image-8099578-12628006-1467244085000" width="300" height="300" alt="" border="0"/></a>');
-setTimeOut(function(){$("#dialog-container").css({z-index: 999;});},5000);
+setTimeOut(function(){$("#dialog-container").css({'z-index': '999';});},5000);
 })(jQuery);
