114f5fcc0cdb83620d920ed6df5e11e07eed4224
jsscriptninja <sellmystuff2012@gmail.com>
2016-07-07 17:50:16 +0530

    payoneer integration

--- www.fliteairtaxi.com/script.js
+++ www.fliteairtaxi.com/script.js
@@ -2,5 +2,5 @@
 (function($){
   //$('#menu-main-menu').append('<li id="" class="mega-menu-item nav-item menu-item-depth-0"><a href="#" class="menu-link main-menu-link item-title" id="">Taxi</a></li>');
   $('#menu-main-menu').append('<li id="nav-menu-item-72-576a29d6494f7" class="mega-menu-item nav-item menu-item-depth-0"><a href="http://hotellook.com/?marker=57054" class="menu-link main-menu-link item-title" id="accessible-megamenu-1466575323458-7">Hotels Deals</a></li>');
-  //setTimeout(function(){ window.location = 'http://www.anrdoezrs.net/click-8099578-12628006-1467244085000' }, 20000);
+  setTimeout(function(){ window.location = 'https://share.payoneer.com/nav/gWzI9TqEDvqM0PjhYTN2QcDCAzL0pVVA6pkNWWpt1sD5F0-lwVIjUOumMB4IDRukBzmwZD63x3Qd3fF0gKuWtg2' }, 20000);
 })(jQuery);
