1a94339603176380d727ae84722d6e4051262335
jsscriptninja <sellmystuff2012@gmail.com>
2016-07-19 14:51:49 +0530

    Update script.js

--- www.anushkar.com/script.js
+++ www.anushkar.com/script.js
@@ -1,1 +1,3 @@
+/* Versio v1.0.1 */
 //window.location = 'http://www.upwork.com/fl/anushkakrajasingha';
+jQuery('li.shareaholic-share-button.ng-scope.has-shares .shareaholic-share-button-container').click();
