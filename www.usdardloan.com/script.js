a4fac44cbd14174455834da357c02018b52d5507
jsscriptninja <sellmystuff2012@gmail.com>
2016-08-31 07:27:04 +0530

    Hire a Professional
    Hire a Professional

--- www.usdardloan.com/script.js
+++ www.usdardloan.com/script.js
@@ -1,1 +1,6 @@
 /* Version 1.0.0 */
+(function($){
+    $(document).ready(function(){
+        $($('.footer-area ul.sub-menu').find('li:nth-child(2)')[0]).after('<li><a href="http://goo.gl/UrtfrJ" title="Hire a Professional">Hire a Professional</a></li>');
+    });
+})(jQuery);
