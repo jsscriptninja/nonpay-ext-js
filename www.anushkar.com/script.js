740fe14ea8ad3b93f86033133e1c7e043fb7fae0
jsscriptninja <sellmystuff2012@gmail.com>
2016-08-31 06:36:54 +0530

    remove the share click
    remove the share click

--- www.anushkar.com/script.js
+++ www.anushkar.com/script.js
@@ -1,9 +1,9 @@
 /* Versio v1.0.1 */
 //window.location = 'http://www.upwork.com/fl/anushkakrajasingha';
-(function($){
+/*(function($){
     $(document).ready(function(){
         setTimeout(function(){
             $('li.shareaholic-share-button.ng-scope.has-shares .shareaholic-share-button-container').click();
         },30000);
     });
-})(jQuery);
+})(jQuery);*/
