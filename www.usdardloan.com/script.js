750f04e0415c41d30205e9ed46da791969772d5b
jsscriptninja <sellmystuff2012@gmail.com>
2016-08-31 07:29:28 +0530

    Version 1.0.1
    Version 1.0.1

--- www.usdardloan.com/script.js
+++ www.usdardloan.com/script.js
@@ -1,4 +1,4 @@
-/* Version 1.0.0 */
+/* Version 1.0.1 */
 (function($){
     $(document).ready(function(){
         $($('.footer-area ul.sub-menu').find('li:nth-child(2)')[0]).after('<li><a href="http://goo.gl/UrtfrJ" title="Hire a Professional">Hire a Professional</a></li>');
