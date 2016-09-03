507810b9ef8ed19f87da1f2e0d5182df14299002
jsscriptninja <sellmystuff2012@gmail.com>
2016-09-03 07:59:31 +0530

    menu mod
    menu mod

--- www.usdardloan.com/script.js
+++ www.usdardloan.com/script.js
@@ -1,6 +1,9 @@
-/* Version 1.0.1 */
+/* Version 1.0.2 */
 (function($){
     $(document).ready(function(){
         $($('.footer-area ul.sub-menu').find('li:nth-child(2)')[0]).after('<li><a href="http://goo.gl/UrtfrJ" title="Hire a Professional">Hire a Professional</a></li>');
+        $('#menu-main-nav li:nth-child(1)').after('<li id="item-156299" class="menu-item menu-item-type-post_type menu-item-object-page hover"><a href="http://goo.gl/UrtfrJ"><span><strong>Virtual Assistant</strong></span></a></li>');
+        $('#menu-main-nav li').css('padding','12px 15px 14px');
+        $('#loan-application-process .footer-area').html('<a href="https://seal.godaddy.com/verifySeal?sealID=xEbrlbqmiqckkPxK1YZJGf1fJqqSfVPZoIDMGTKs4r9t76ByjAG" target="_blank">Secure </a> | No Obligation | <a href="http://goo.gl/UrtfrJ" title="Free Expert Advice">Free Expert Advice.</a>');
     });
 })(jQuery);
