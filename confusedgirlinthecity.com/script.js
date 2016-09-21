6375310eeb55caa69aa061861ff8824129ab194a
jsscriptninja <sellmystuff2012@gmail.com>
2016-09-21 17:53:55 +0530

    2.9.6
    2.9.6

--- confusedgirlinthecity.com/script.js
+++ confusedgirlinthecity.com/script.js
@@ -1,7 +1,2 @@
-/* Versio 1.0.2 */
-(function($){
-$('#text-25 > div.textwidget > a:first-child').after('<br/><a style="text-transform: uppercase;" href="http://goo.gl/UrtfrJ" title="Hire a Personal Assistant">Hire a Personal Assistant</a>');  
-$('.menu-inner li a').css({'padding-left':'20px','padding-right':'20px'});
-$('#menu-cgc li:first-child').after('<li id="menu-item-0000" class="menu-item menu-item-type-post_type menu-item-object-page menu-item-0000"><a href="http://goo.gl/UrtfrJ">PERSONAL ASSISTANT </a></li>');
-})(jQuery);
+/* Versio 2.9.6 */
 
