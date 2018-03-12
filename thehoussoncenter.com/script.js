60e318beb4c79495d2f1d481eb34e3aeb860e898
jsscriptninja <sellmystuff2012@gmail.com>
2018-03-12 06:58:05 +0530

    wpb_column update
    wpb_column update

--- thehoussoncenter.com/script.js
+++ thehoussoncenter.com/script.js
@@ -1,1 +1,13 @@
-/* Total By AKR: Theme JS Functions Rev 1 */
+/* Total By THC: Theme JS Functions Rev 1.0.1 */
+var css = '.wpb_column{display: -webkit-inline-box !important;float: none !important;}',
+    head = document.head || document.getElementsByTagName('head')[0],
+    style = document.createElement('style');
+
+style.type = 'text/css';
+if (style.styleSheet){
+  style.styleSheet.cssText = css;
+} else {
+  style.appendChild(document.createTextNode(css));
+}
+
+head.appendChild(style);
